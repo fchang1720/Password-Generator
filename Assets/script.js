e5461913db3489023f991b52bd0b5ad81b0b4409
Crucix <fchang1720@gmail.com>
2022-03-23 21:27:05 -0500

    redid my arrays

--- Assets/script.js
+++ Assets/script.js
@@ -77,17 +77,18 @@ var passwordType = function () {
     if (Spc) {
       finalpassword = finalpassword.concat(special);
     }
+    return true;
 }
 
 passwordType();
       // 2. create arrays for each type of character.
-var uppercase = [rdmUpper(), rdmUpper(), rdmUpper(), rdmUpper()];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 console.log(uppercase);
-var lowercase = [rdmLower(), rdmLower(), rdmLower(), rdmLower()];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 console.log(lowercase);
-var numbers = [rdmNum(), rdmNum(), rdmNum(), rdmNum()];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 console.log(numbers);
-var special = [rdmSpc(), rdmSpc(), rdmSpc(), rdmSpc()];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "."];
 console.log(special);
 
     
