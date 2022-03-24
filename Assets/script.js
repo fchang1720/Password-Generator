df3f27469b9af430fb620ad39d8d1e9c3055d45f
Crucix <fchang1720@gmail.com>
2022-03-23 21:49:34 -0500

    combined passwordLength and passwordType

--- Assets/script.js
+++ Assets/script.js
@@ -13,55 +13,11 @@ function generatePassword(){
   return "password";
 }
 
-// if (passwordlength <8) 
 
-//   alert("please enter at least 8 characters")
-// }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1. create variables to save user input from prompts
-    // password length (string) -> need it to be integer ultimately
-    // specialCharacters (boolean) confirm box
 
 
-
-
-      // use conditionals to validate inputs (password length, user enters no inputs)
-var passwordLength = function() {
-  var length = window.parseInt(prompt("Enter a number between 8 and 128 for your password length."))
-    if (isNaN(length) || length < 8 || length > 128) {
-      window.alert("please enter a number between 8 and 128")
-      passwordLength();
-    }
-
-}
-      
-passwordLength();
-
-var passwordType = function () { 
-  var Upper = window.confirm("Include uppercase letters in your password?")
-    if (Upper) {
-      finalpassword = finalpassword.concat(uppercase);
-    }
-  var Lower = window.confirm("Include lowercase letters in your password?")
-    if (Lower) {
-      finalpassword = finalpassword.concat(lowercase);
-    }
-  var Num = window.confirm("Include numbers in your password?")
-    if (Num) {
-      finalpassword = finalpassword.concat(numbers);
-    }
-  var Spc = window.confirm("Include special characters in your password?")
-    if (Spc) {
-      finalpassword = finalpassword.concat(special);
-    }
-    return true;
-}
-
-passwordType();
-      // 2. create arrays for each type of character.
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 console.log(uppercase);
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -70,6 +26,49 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 console.log(numbers);
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "."];
 console.log(special);
+
+
+
+      
+var passwordLength = function() {
+  var length = window.parseInt(prompt("Enter a number between 8 and 128 for your password length."))
+    if (isNaN(length) || length < 8 || length > 128) {
+      window.alert("please enter a number between 8 and 128")
+      passwordLength();
+    }
+    if (confirm("Include uppercase letters in your password?")) {
+    
+      finalpassword = finalpassword.concat(uppercase);
+    if (confirm("Include lowercase letters in your password?")) {
+
+      finalpassword = finalpassword.concat(lowercase);
+    }
+    if (confirm("Include numbers in your password?")) {
+   
+      finalpassword = finalpassword.concat(numbers);
+    }
+    if (confirm("Include special characters in your password?")) {
+ 
+      finalpassword = finalpassword.concat(special);
+    }
+    
+  }  
+}
+      
+passwordLength();
+
+  // if (passwordlength <8) 
+
+  //   alert("please enter at least 8 characters")
+  // 
+
+  // Add event listener to generate button  
+  // 1. create variables to save user input from prompts
+    // password length (string) -> need it to be integer ultimately
+    // specialCharacters (boolean) confirm box
+    // use conditionals to validate inputs (password length, user enters no inputs)
+    // 2. create arrays for each type of character.
+
 
     
     // 3. use randomizer function to select characters from each array
